e19739ccd5e3aa52d06791484d94b46d2afb4d65
Hassan Mehmood <hassan.mehmood@afiniti.com>
2019-01-10 18:07:28 +0500

    Material UI added

--- poc/src/components/main/main.jsx
+++ poc/src/components/main/main.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './main.css'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
+const NavBar = () => {
+  return(
+      <div>
+      <AppBar position="static">
+          <Toolbar>
+              <Typography variant="title" color="inherit">
+              React & Material-UI Sample Application
+              </Typography>
+          </Toolbar>
+      </AppBar>
+      </div>
+  )
+}
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        Main
-      </div>
+      <NavBar>
+        
+      </NavBar>
     );
   }
 }
